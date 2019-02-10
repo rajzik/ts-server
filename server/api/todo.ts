f1534a3abd29a553e5247a0a53d9bad5445ed6cd
Jan Šilhan <silhanu@gmail.com>
2019-02-10 16:00:10 +0100

    Updated Todo API

--- server/api/todo.ts
+++ server/api/todo.ts
@@ -2,39 +2,51 @@ import { TodoDatabase } from '../database';
 
 import { Router, Request, Response } from 'express';
 
-
+interface ITodo {
+  _id: string;
+  text: string;
+  done: boolean;
+}
 
 export async function init() {
   await TodoDatabase.loadDatabaseAsync();
-
 }
-
-
 
 const router = Router();
 
 router.get('/todo', async (_: Request, res: Response) => {
   res.json(await TodoDatabase.findAsync({}));
-})
+});
 
 router.post('/todo', async (req: Request, res: Response) => {
   console.log(req.body);
   res.json(await TodoDatabase.insertAsync(req.body));
-})
+});
+
+router.put('/todo/:id', async (req: Request, res: Response) => {
+  const _id = req.params.id;
+
+  try {
+    await TodoDatabase.updateAsync({ _id }, req.body, {});
+    res.json((await TodoDatabase.findAsync({ _id }))[0]);
+  } catch (e) {
+    res.status(400);
+  }
+});
+
+router.patch('/todo/:id', async (req: Request, res: Response) => {
+  const _id = req.params.id;
+  try {
+    const todo: ITodo[] = await TodoDatabase.findAsync({ _id });
+    await TodoDatabase.updateAsync({ _id }, { $set: { done: !todo[0].done} }, {});
+    if (todo[0]) {
+      res.json((await TodoDatabase.findAsync({ _id }))[0]);
+    } else {
+      res.status(404);
+    }
+  } catch (e) {
+    res.status(400);
+  }
+});
 
 export { router };
-// export async function getTodo(data: Context) {
-//   const a = await TodoDatabase.findAsync({ _id: data.params.id });
-//   if (a.length > 0) return a[0];
-//   return status(404).send("Not Found!");
-// }
-
-// export async function getAllTodos() {
-//   return await TodoDatabase.findAsync({});
-// }
-
-
-// export async function addTodo(ctx: Context) {
-//   const { data } = ctx;
-//   return status(201).json(await TodoDatabase.insertAsync(data));
-// }
